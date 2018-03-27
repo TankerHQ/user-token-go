9c2ffb8e41594be6e2ff8719b5f273fe42a2cf96
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-03-27 13:55:29 +0200

    my turn

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-LIBSODIUM_VERSION=1.0.16
+LIBSODIUM_VERSION="1.0.16"
+LIBSODIUM_GO_SHA1="692ddacd45e46b2c7512890f032b39d019a5464b"
 
 echo "Installing libsodium"
 wget -q https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION/libsodium-$LIBSODIUM_VERSION.tar.gz
@@ -10,18 +11,21 @@ tar -xzf libsodium-$LIBSODIUM_VERSION.tar.gz
 (
     cd libsodium-$LIBSODIUM_VERSION/
     ./configure
-    make
+    make -j $(nproc)
     sudo make install
 )
 
 echo "Install go dependencies"
-go get -t ./...
+go get -v -t ./...
 
 echo "Build libsodium-go dependency"
 (
   cd $GOPATH/src/github.com/GoKillers/libsodium-go/
+  git reset --hard $LIBSODIUM_GO_SHA1
   # clang can't compile this Oo
   export CC=gcc CXX=g++
   ./build.sh
   go install -v ./...
 )
+
+go test -v ./...
