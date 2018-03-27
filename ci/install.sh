d7b15c990127b6ab1626e19be1723209206175ee
Maxime RETY <maxime.rety@gmail.com>
2018-03-27 14:12:27 +0200

    CI (#1)
    Enable Travis CI.
    
    First try!

---
+++ ci/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+LIBSODIUM_VERSION="1.0.16"
+LIBSODIUM_GO_SHA1="692ddacd45e46b2c7512890f032b39d019a5464b"
+
+echo "Installing libsodium"
+wget -q https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION/libsodium-$LIBSODIUM_VERSION.tar.gz
+tar -xzf libsodium-$LIBSODIUM_VERSION.tar.gz
+(
+    cd libsodium-$LIBSODIUM_VERSION/
+    ./configure --disable-shared
+    make -j $(nproc)
+    sudo make install
+)
+
+echo "Install go dependencies"
+go get -v -t ./...
+
+echo "Build libsodium-go dependency"
+(
+  cd $GOPATH/src/github.com/GoKillers/libsodium-go/
+  git reset --hard $LIBSODIUM_GO_SHA1
+  # clang can't compile this Oo
+  export CC=gcc CXX=g++
+  ./build.sh
+  go install -v ./...
+)
